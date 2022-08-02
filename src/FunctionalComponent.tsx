7e88edb499fbcb15a4b2e9a2ec54694898c1be7f
nhoxuan <nhoxuan@atlassian.com>
2022-08-03 09:29:05 +1000

    add Profiler

--- src/FunctionalComponent.tsx
+++ src/FunctionalComponent.tsx
@@ -1,10 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, Profiler } from 'react';
 let cacheFn: any;
 
+function onRenderCallback(
+  id: any, // the "id" prop of the Profiler tree that has just committed
+  phase: any, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+  actualDuration: any, // time spent rendering the committed update
+  baseDuration: any, // estimated time to render the entire subtree without memoization
+  startTime: any, // when React began rendering this update
+  commitTime: any, // when React committed this update
+  interactions: any // the Set of interactions belonging to this update
+) {
+    console.error({id, phase, actualDuration, baseDuration,startTime,commitTime,interactions});
+}
 function FunctionalComponent() {
     // Declare a new state variable, which we'll call "count"
     const [count, setCount] = useState(0);
-    console.error("FunctionalComponent");
+    // console.error("FunctionalComponent");
     // const fn = function () {
     //     console.error("this is fn");
     // };
@@ -16,16 +27,17 @@ function FunctionalComponent() {
     useEffect(() => {
         // Update the document title using the browser API
         document.title = `You clicked ${count} times`;
-        console.error('run effect');
+        // console.error(`run effect in FunctionalComponent when count = ${count}`);
         return () => {
-            console.error('cleanup effect');
+            // console.error(`cleanup effect when count = ${count}`);
         }
     });
     return (
         <div>
             <p>You clicked {count} times</p>
             <button onClick={() => setCount(count + 1)}>Click me</button>
-            {count == 0 || count == 5 ? null : <ComponentA />}
+            {count == 0 || count == 5 ? null : <Profiler id='ComponentA' onRender={onRenderCallback}><ComponentA /></Profiler>}
+            {/* {count == 0 || count == 5 ? null : <ComponentB />} */}
         </div>
     );
 }
@@ -33,16 +45,55 @@ function FunctionalComponent() {
 function ComponentA() {
     // Declare a new state variable, which we'll call "count"
     const [count, setCount] = useState(0);
-    console.error(`component A: ${count}`);
+    const renderCount = useRef(0);
+    // console.error(`component A: ${count}`);
     useEffect(() => {
         let ignore = false;
-        console.error('run effect from component A FunctionalComponent');
+        // console.error('run effect from component A FunctionalComponent');
         setTimeout(()=>{
-            if (!ignore) console.error('not ignore-----------------');
+            // if (!ignore) console.error('not ignore-----------------');
+        }, 2000);
+        defineOperation();
+        return () => {
+            ignore = true;
+            // console.error('clean effect from component A FunctionalComponent');
+        }
+    }, [count]);
+
+    const [operationResult, setOperationResult] = useState('');
+    const defineOperation = () => {
+        const remainder = count % 2;
+        switch (remainder) {
+            case 0:
+                setOperationResult('Count (click) is even number');
+                break;
+            case 1:
+                setOperationResult('Count (click) is odd number');
+                break;
+        }
+    }
+    return (
+        <div>
+            <p>You clicked {count} times. I have been rendered {++renderCount.current} times </p>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+            <p>{operationResult}</p>
+        </div>
+    );
+}
+
+function ComponentB() {
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
+    // console.error(`component B: ${count}`);
+    useEffect(() => {
+        let ignore = false;
+        // console.error('run effect from component B FunctionalComponent');
+        setTimeout(()=>{
+            // if (!ignore) console.error('not ignore-----------------');
         }, 2000);
         return () => {
             ignore = true;
-            console.error('clean effect from component A FuncComp');
+            // console.error('clean effect from component B FunctionalComponent');
         }
     }, []);
     return (
